0e7759b007bef1313058213fb19450dcf41ff32a
Juliya <yula_sakura@list.ru>
2022-12-06 00:06:21 +0300

    Commit

--- src/components/ElemOfChart.jsx
+++ src/components/ElemOfChart.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import '../App.css'
+import { MdDisabledByDefault } from "react-icons/md";
+import { RxPencil1 } from "react-icons/rx";
 
 const ElemOfChart = (props) => {
     
   return (
     <div className="chart">
-    <div>
       {props.items.map((item) => (
         <div className='elem-of-chart' key={item.id}>
-        <div className='date'>
-        {item.date.getDate() + "." + 
-        (item.date.getMonth() + 1) + "." + 
-        item.date.getFullYear().toString().slice(2)}</div>
-        <div className='distance'>{item.distance}</div>
-        <button className='btn-edit' onClick={() => props.edit(item.id)}>Edit</button>
-        <button className='btn-rmv' onClick={() => props.remove(item.id)}>Remove</button></div>
-      ))}
-    </div>
+            <div className='date'>
+                {item.date.getDate() + "." + 
+                (item.date.getMonth() + 1) + "." + 
+                item.date.getFullYear().toString().slice(2)}
+            </div>
+            <div className='distance'>{item.distance}</div>
+            <div className='icons'>
+            <RxPencil1 className='btn-edit' onClick={() => props.edit(item.id)}/>
+            <MdDisabledByDefault className='btn-rmv' onClick={() => props.remove(item.id)}/>
+            </div>
+        </div>))
+      }
     </div>
   );
 };
